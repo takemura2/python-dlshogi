6415212f17f9659a81e1e5bca3e102b0690f994e
takemura2 <takemura2@gmail.com>
2020-01-30 23:29:10 +0900

    hoge

--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
-開発モードでパッケージをインストール
+開発モードでパッケージをインストール方法
 pip install --no-cache-dir -e .
+
+github からインストールする方法
+pip install git+https://github.com/takemura2/python-dlshogi
+
+アンインストール
+pip uninstall pydlshogi
--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
 ﻿from setuptools import setup
-setup()
+setup(
+    name='pydlshogi',
+    version='1.0.0',
+    author='たけむら',
+    description='たけむら将棋',
+    packages=['pydlshogi'],
+    install_requires=['cupy == 7.0.0',
+                      'chainer == 7.0.0',
+                      'python-shogi == 1.0.9'
+                      ],
+    scripts=[],
+
+)
