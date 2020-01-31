2598a2533e8859d28f653e40c54afca9e1bbc7e2
takemura2 <takemura2@gmail.com>
2020-01-31 22:31:19 +0900

    hoge

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-﻿from setuptools import setup
+﻿from setuptools import setup, find_packages
+
+
 setup(
     name='pydlshogi',
     version='1.0.0',
     author='たけむら',
+    url='https://github.com/takemura2/python-dlshogi',
+    author_email='takemura2@gmail.com',
     description='たけむら将棋',
-    packages=['pydlshogi'],
+    license='MIT',
+    keywords='game shogi deeplearning',
+    packages=find_packages("pydlshogi"),
     install_requires=['cupy == 7.0.0',
                       'chainer == 7.0.0',
                       'python-shogi == 1.0.9'
