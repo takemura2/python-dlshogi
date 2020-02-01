7748fa900f729f8f14e4613ed5075c522849e4b1
takemura2 <takemura2@gmail.com>
2020-02-01 13:06:10 +0900

    hoge

--- hello.py
+++
@@ -1,4 +0,0 @@
-import os
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-print(current_dir)
--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 ﻿from setuptools import setup, find_packages
 
+_package_name = 'pydlshogi'
+
+_version = '1.0.0'
+
+_packages = find_packages(
+    where='./',
+    exclude=[
+        'utils'
+    ]
+)
+
 setup(
-    name='pydlshogi',
-    version='1.0.0',
+    name=_package_name,
+    version=_version,
     author='たけむら',
     url='https://github.com/takemura2/python-dlshogi',
     author_email='takemura2@gmail.com',
     description='たけむら将棋',
     license='MIT',
     keywords='game shogi deeplearning',
-    # packages=find_packages(where='./',
-    #                        exclude=[
-    #                            'utils'
-    #                        ]
-    #                        ),
-    packages=['pydlshogi'],
-    package_dir={'pydlshogi': 'pydlshogi'},
+    packages=_packages,
     package_data={'pydlshogi': ['model_data/*']},
     install_requires=['cupy == 7.0.0',
                       'chainer == 7.0.0',
